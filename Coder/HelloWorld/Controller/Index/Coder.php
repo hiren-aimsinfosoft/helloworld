67f7440ba2a5d103f50e711c7991db7b0b39e8b2
Hiren Patel <50257430+hiren-aimsinfosoft@users.noreply.github.com>
2020-03-14 17:58:38 +0530

    Add files via upload

---
+++ Coder/HelloWorld/Controller/Index/Coder.php
@@ -0,0 +1,21 @@
+<?php
+namespace Coder\HelloWorld\Controller\Index;
+
+class Coder extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+		echo "Hello Coding World";
+		exit;
+	}
+}
